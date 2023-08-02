9956b910aaa799142c0ef703af2078858139c9c7
shivani3301 <ksweedan7@icloud.com>
2023-08-02 09:07:22 +0300

    JSON file

--- src/players.js
+++ src/players.js
@@ -1,14 +1,52 @@
-import torres from "./imgs/ferranTorres.png"
-import lewandowski from "./imgs/lewandowski.png"
-import lewandowskiTwo from "./imgs/lewandowski2.webp"
-import messi from "./imgs/messi.avif"
-import raphinha from "./imgs/raphinha.webp"
+import lewandowski from "./imgs/lewandowski.png";
+import lewandowskiTwo from "./imgs/lewandowski2.webp";
+import torres from "./imgs/ferranTorres.png";
+import messi from "./imgs/messi.avif";
+import raphinha from "./imgs/raphinha.webp";
+import pedri from "./imgs/pedri.webp";
 
 // import images^^
 // JSON array of objects
 
 export default [
-    {
-
-    }
-]
+  {
+    name: "Lewandowski",
+    team: "FC Barcelona",
+    nationality: "Polish",
+    jerseyNumber: 9,
+    age: 34,
+    image: lewandowski,
+  },
+  {
+    name: "Torres",
+    team: "FC Barcelona",
+    nationality: "Spanish",
+    jerseyNumber: 11,
+    age: 23,
+    image: torres,
+  },
+  {
+    name: "Messi",
+    team: "FC Barcelona",
+    nationality: "Argentinian",
+    jerseyNumber: 10,
+    age: 36,
+    image: messi,
+  },
+  {
+    name: "Raphinha",
+    team: "FC Barcelona",
+    nationality: "Brasilian",
+    jerseyNumber: 22,
+    age: 26,
+    image: raphinha,
+  },
+  {
+    name: "Pedri",
+    team: "FC Barcelona",
+    nationality: "Spanish",
+    jerseyNumber: 8,
+    age: 20,
+    image: pedri,
+  },
+];
