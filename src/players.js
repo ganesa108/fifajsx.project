fe15cfc92943be7dd358d01b1b2e7b86481fda63
shivani3301 <ksweedan7@icloud.com>
2023-08-02 08:49:43 +0300

    environmentSetup

---
+++ src/players.js
@@ -0,0 +1,14 @@
+import torres from "./imgs/ferranTorres.png"
+import lewandowski from "./imgs/lewandowski.png"
+import lewandowskiTwo from "./imgs/lewandowski2.webp"
+import messi from "./imgs/messi.avif"
+import raphinha from "./imgs/raphinha.webp"
+
+// import images^^
+// JSON array of objects
+
+export default [
+    {
+
+    }
+]
